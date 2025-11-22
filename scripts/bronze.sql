94d106edaea0641575b479dd3e44be75ac1e6067
Rodricks Prem Kumar <rodricks.premkumar@gmail.com>
2025-11-22 19:22:26 +0530

    DDL

---
+++ scripts/bronze.sql
@@ -0,0 +1,81 @@
+use datawarehouse;
+go
+if OBJECT_ID('bronze.crm_cust_info','U') is NOT NULL
+ drop table bronze.crm_cust_info;
+go
+create table bronze.crm_cust_info
+(
+
+    cst_id int,
+    cst_key NVARCHAR(50),
+    cst_first_name NVARCHAR(50),
+    cst_last_name NVARCHAR(50),
+    cst_material_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date date
+);
+
+go
+if OBJECT_ID('bronze.crm_prd_info','U') is NOT NULL
+ drop table bronze.crm_cust_info;
+go
+create table bronze.crm_prd_info
+(
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt datetime,
+    prd_end_dt datetime 
+);
+go
+
+if OBJECT_ID('bronze.crm_cust_info','U') is NOT NULL
+ drop table bronze.crm_cust_info;
+go
+create table bronze.crm_sales_details
+(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+go
+if OBJECT_ID('bronze.erp_loc_a101','U') is NOT NULL
+ drop table bronze.crm_cust_info;
+go
+
+create TABLE bronze.erp_loc_a101 (
+    CID NVARCHAR(50),
+    CNTRY NVARCHAR(50)
+);
+
+go
+if OBJECT_ID('bronze.erp_cust_az12','U') is NOT NULL
+ drop table bronze.crm_cust_info;
+go
+
+create TABLE bronze.erp_cust_az12 (
+    CID NVARCHAR(50),
+    BDATE DATE,
+    GEN NVARCHAR(50)
+);
+
+go
+
+if OBJECT_ID('bronze.erp_px_cat_g1v2','U') is NOT NULL
+ drop table bronze.crm_cust_info;
+go
+create TABLE bronze.erp_px_cat_g1v2 (
+    ID  NVARCHAR(50),
+    CAT NVARCHAR(50),
+    SUBCAT NVARCHAR(50),
+    MAINTENANCE NVARCHAR(50)
+);
